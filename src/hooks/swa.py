e80094965cb86c76cb64f64660e793660974eafa
Laurent Dillard <laurent.dillard@gmail.com>
2020-09-30 12:45:39 +0200

    Fix in tabulation

--- src/hooks/swa.py
+++ src/hooks/swa.py
@@ -3,23 +3,23 @@ from torch.optim import swa_utils
 from copy import deepcopy
 
 class SWA(Hook):
-	def __init__(self, epoch_start, swa_lr):
-		self.epoch_start = epoch_start
-		self.swa_lr = swa_lr
-		self.anneal_epoch = anneal_epoch
+    def __init__(self, epoch_start, swa_lr):
+        self.epoch_start = epoch_start
+        self.swa_lr = swa_lr
+        self.anneal_epoch = anneal_epoch
 
-	def train_begin(self):
-		self.swa_model = swa_utils.AveragedModel(self.trainer.model)
-		swa_epochs = self.trainer.config.OPTIM.EPOCH - self.epoch_start
-		anneal_epoch = int(swa_epochs*self.anneal_epoch)
-		self.swa_scheduler = swa_utils.SWALR(self.trainer.optim, swa_lr=self.swa_lr, anneal_epoch=anneal_epoch)
+    def train_begin(self):
+        self.swa_model = swa_utils.AveragedModel(self.trainer.model)
+        swa_epochs = self.trainer.config.OPTIM.EPOCH - self.epoch_start
+        anneal_epoch = int(swa_epochs*self.anneal_epoch)
+        self.swa_scheduler = swa_utils.SWALR(self.trainer.optim, swa_lr=self.swa_lr, anneal_epoch=anneal_epoch)
 
-	def epoch_end(self):
-		if self.trainer.epoch > self.epoch_start:
-          self.swa_model.update_parameters(self.trainer.model)
-          self.swa_scheduler.step()
+    def epoch_end(self):
+        if self.trainer.epoch > self.epoch_start:
+            self.swa_model.update_parameters(self.trainer.model)
+            self.swa_scheduler.step()
 
     def train_end(self):
-    	swa_utils.update_bn(self.trainer.train_loader, self.swa_model)
-    	# Copying parameters back to original model
-    	self.trainer.model = deepcopy(self.swa_model.module)
+        swa_utils.update_bn(self.trainer.train_loader, self.swa_model)
+        # Copying parameters back to original model
+        self.trainer.model = deepcopy(self.swa_model.module)
