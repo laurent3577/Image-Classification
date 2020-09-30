c768dfa8471dd3753c60fee9506073df861b4a0e
Laurent Dillard <laurent.dillard@gmail.com>
2020-09-30 20:50:56 +0200

    Add resnext to build model

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -7,5 +7,11 @@ def build_model(config):
 		return resnet34(config.MODEL.PRETRAINED, num_classes=config.MODEL.NUM_CLASSES)
 	elif config.MODEL.NAME == "resnet50":
 		return resnet50(config.MODEL.PRETRAINED, num_classes=config.MODEL.NUM_CLASSES)
+	elif config.MODEL.NAME == "resnet101":
+		return resnet101(config.MODEL.PRETRAINED, num_classes=config.MODEL.NUM_CLASSES)
+	elif config.MODEL.NAME == "resnext50_32x4d":
+		return resnext50_32x4d(config.MODEL.PRETRAINED, num_classes=config.MODEL.NUM_CLASSES)
+	elif config.MODEL.NAME == "resnext101_32x8d":
+		return resnext101_32x8d(config.MODEL.PRETRAINED, num_classes=config.MODEL.NUM_CLASSES)
 	else:
 		raise ValueError("Not supported model: {}".format(config.MODEL.NAME))
