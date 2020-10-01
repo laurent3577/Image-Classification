5e78ec098c345be240e019abf42e101a0fc0d02c
Laurent Dillard <laurent.dillard@gmail.com>
2020-10-01 13:18:23 +0200

    Add densenet

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,4 +1,5 @@
 from .resnet import *
+from .densenet import *
 
 def build_model(config):
 	if config.MODEL.NAME == "resnet18":
@@ -13,5 +14,13 @@ def build_model(config):
 		return resnext50_32x4d(config.MODEL.PRETRAINED, num_classes=config.MODEL.NUM_CLASSES)
 	elif config.MODEL.NAME == "resnext101_32x8d":
 		return resnext101_32x8d(config.MODEL.PRETRAINED, num_classes=config.MODEL.NUM_CLASSES)
+	elif config.MODEL.NAME == "densenet121":
+		return densenet121(config.MODEL.PRETRAINED, num_classes=config.MODEL.NUM_CLASSES)
+	elif config.MODEL.NAME == "densenet161":
+		return densenet161(config.MODEL.PRETRAINED, num_classes=config.MODEL.NUM_CLASSES)
+	elif config.MODEL.NAME == "densenet169":
+		return densenet169(config.MODEL.PRETRAINED, num_classes=config.MODEL.NUM_CLASSES)
+	elif config.MODEL.NAME == "densenet201":
+		return densenet201(config.MODEL.PRETRAINED, num_classes=config.MODEL.NUM_CLASSES)
 	else:
 		raise ValueError("Not supported model: {}".format(config.MODEL.NAME))
