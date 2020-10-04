061067b3d50119144003197a5cf99e9216f4a7cc
Laurent Dillard <laurent.dillard@gmail.com>
2020-10-04 15:48:54 +0200

    remove print

--- src/models/resnet.py
+++ src/models/resnet.py
@@ -217,7 +217,6 @@ class ResNet(nn.Module):
         x = self.maxpool(x)
 
         x = self.layer1(x)
-        print(x)
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
