4d2a17ddc1e3bbf8383e25d34867c965c6833944
Laurent Dillard <laurent.dillard@gmail.com>
2020-10-04 14:46:51 +0200

    Add aff param

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -4,17 +4,17 @@ from .efficientnet import *
 
 def build_model(config):
 	if config.MODEL.NAME == "resnet18":
-		return resnet18(config.MODEL.PRETRAINED, num_classes=config.MODEL.NUM_CLASSES)
+		return resnet18(config.MODEL.PRETRAINED, num_classes=config.MODEL.NUM_CLASSES, aff=config.MODEL.AFF)
 	elif config.MODEL.NAME == "resnet34":
-		return resnet34(config.MODEL.PRETRAINED, num_classes=config.MODEL.NUM_CLASSES)
+		return resnet34(config.MODEL.PRETRAINED, num_classes=config.MODEL.NUM_CLASSES, aff=config.MODEL.AFF)
 	elif config.MODEL.NAME == "resnet50":
-		return resnet50(config.MODEL.PRETRAINED, num_classes=config.MODEL.NUM_CLASSES)
+		return resnet50(config.MODEL.PRETRAINED, num_classes=config.MODEL.NUM_CLASSES, aff=config.MODEL.AFF)
 	elif config.MODEL.NAME == "resnet101":
-		return resnet101(config.MODEL.PRETRAINED, num_classes=config.MODEL.NUM_CLASSES)
+		return resnet101(config.MODEL.PRETRAINED, num_classes=config.MODEL.NUM_CLASSES, aff=config.MODEL.AFF)
 	elif config.MODEL.NAME == "resnext50_32x4d":
-		return resnext50_32x4d(config.MODEL.PRETRAINED, num_classes=config.MODEL.NUM_CLASSES)
+		return resnext50_32x4d(config.MODEL.PRETRAINED, num_classes=config.MODEL.NUM_CLASSES, aff=config.MODEL.AFF)
 	elif config.MODEL.NAME == "resnext101_32x8d":
-		return resnext101_32x8d(config.MODEL.PRETRAINED, num_classes=config.MODEL.NUM_CLASSES)
+		return resnext101_32x8d(config.MODEL.PRETRAINED, num_classes=config.MODEL.NUM_CLASSES, aff=config.MODEL.AFF)
 	elif config.MODEL.NAME == "densenet121":
 		return densenet121(config.MODEL.PRETRAINED, num_classes=config.MODEL.NUM_CLASSES)
 	elif config.MODEL.NAME == "densenet161":
