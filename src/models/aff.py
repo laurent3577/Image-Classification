a860b14ee81eac8038f290b692b0eae47181a8ee
Laurent Dillard <laurent.dillard@gmail.com>
2020-10-04 14:57:50 +0200

    Fix in aff

--- src/models/aff.py
+++ src/models/aff.py
@@ -7,18 +7,18 @@ def conv1x1block(in_channels, out_channels, use_bn, use_act):
         modules.append(nn.BatchNorm2d(num_features=out_channels))
     if use_act:
         modules.append(nn.LeakyReLU())
-    return nn.ModuleList(modules)
+    return nn.Sequential(*modules)
 
 class MSCam(nn.Module):
 	def __init__(self, in_channels, ratio):
 		super(MSCam, self).__init__()
-		self._global = nn.Sequential([
+		self._global = nn.Sequential(
 			nn.AdaptiveAvgPool2d((1, 1)),
 			conv1x1block(in_channels, int(in_channels/ratio), True, True),
-			conv1x1block(int(in_channels/ratio), in_channels, True, False)])
-		self._local = nn.Sequential([
+			conv1x1block(int(in_channels/ratio), in_channels, True, False))
+		self._local = nn.Sequential(
 			conv1x1block(in_channels, int(in_channels/ratio), True, True),
-			conv1x1block(int(in_channels/ratio), in_channels, True, False)])
+			conv1x1block(int(in_channels/ratio), in_channels, True, False))
 
 	def forward(self, x):
 		out = x * torch.sigmoid(self._local(x) + self._global(x))
