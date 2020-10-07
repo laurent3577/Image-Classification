7532625ee31bf22c8869a5833f8dd39f7b2023b8
Laurent Dillard <laurent.dillard@gmail.com>
2020-10-07 16:52:41 +0200

    Fix in build dataset

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -2,17 +2,16 @@ from .dataset import CIFAR100, CIFAR10
 
 def build_dataset(config, split, transform=None, target_transform=None):
 	if config.DATASET.NAME == "CIFAR100":
-		CIFAR100(
+		return CIFAR100(
 			data_dir=config.DATASET.ROOT,
 			split=split,
 			transforms=transform,
 			target_transform=target_transform)
 	elif config.DATASET.NAME == "CIFAR10":
-		CIFAR10(
+		return CIFAR10(
 			data_dir=config.DATASET.ROOT,
 			split=split,
 			transforms=transform,
 			target_transform=target_transform)
 	else:
 		raise ValueError("Not supported dataset: {}".format(config.DATASET.NAME))
-	return dataset
