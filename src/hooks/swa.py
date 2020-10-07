25c31fd80e1c4fec6964289e58a6169efb290189
Laurent Dillard <laurent.dillard@gmail.com>
2020-10-07 22:49:57 +0200

    Fix SWA with data refacto

--- src/hooks/swa.py
+++ src/hooks/swa.py
@@ -1,6 +1,7 @@
 from . import Hook
 from torch.optim import swa_utils
 from copy import deepcopy
+import torch
 
 class SWA(Hook):
     def __init__(self, epoch_start, swa_lr, anneal_epoch):
@@ -20,8 +21,34 @@ class SWA(Hook):
             self.swa_model.update_parameters(self.trainer.model)
             self.swa_scheduler.step()
 
+    def update_bn(self):
+        momenta = {}
+        for module in self.swa_model.modules():
+            if isinstance(module, torch.nn.modules.batchnorm._BatchNorm):
+                module.running_mean = torch.zeros_like(module.running_mean)
+                module.running_var = torch.ones_like(module.running_var)
+                momenta[module] = module.momentum
+
+        if not momenta:
+            return
+
+        was_training = self.swa_model.training
+        self.swa_model.train()
+        for module in momenta.keys():
+            module.momentum = None
+            module.num_batches_tracked *= 0
+
+        for input in self.trainer.train_loader:
+            input = input['img']
+            input = input.to(self.trainer.device)
+            self.swa_model(input)
+
+        for bn_module in momenta.keys():
+            bn_module.momentum = momenta[bn_module]
+        self.swa_model.train(was_training)
+
     def train_end(self):
-        swa_utils.update_bn(self.trainer.train_loader, self.swa_model, self.trainer.device)
+        self.update_bn()
         # Copying parameters back to original model
         self.trainer.model = deepcopy(self.swa_model.module)
         print("Validation results with SWA weights")
